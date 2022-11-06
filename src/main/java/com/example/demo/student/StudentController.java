e313fc20ac5ef7da940f9675aa369edc67bf36fd
00ZRHun <00zrhun@gmail.com>
2022-11-06 22:34:50 +0800

    lets build an API

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,32 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(2+1, "Anna Smith")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable Integer studentId) {
+        System.out.println("studentId = " + studentId);
+
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist"));
+    }
+
+}
